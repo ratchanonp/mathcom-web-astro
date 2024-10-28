f1fcd9049f7d37a8d8fd4a7b46693352732c0ab8
Ratchanon Panmas <28267945+ratchanonp@users.noreply.github.com>
2024-10-28 20:24:13 +0700

    ✨ Improve display of academic rank in Faculty Cards.

--- src/common/components/Card/FacultyCardTH.tsx
+++ src/common/components/Card/FacultyCardTH.tsx
@@ -44,14 +44,14 @@ const FacultyCard = (props: Props) => {
 				</div>
 			)}
 			<div className="p-2.5 font-kanit md:p-5">
-				<h2 className="text-base font-medium md:text-2xl">
-					{first_name_thai} {last_name_thai}
-				</h2>
-				<p className="text-xs capitalize text-gray-400 md:mt-2 md:text-base">
-					{academic_rank
+				<p className="text-xs capitalize text-gray-400 md:text-base">
+					{academic_rank && academic_rank != "none"
 						? academicName[academic_rank as keyof typeof academicName]
 						: staff_type_th[staff_type as keyof typeof staff_type_th]}
 				</p>
+				<h2 className="text-base font-medium md:text-2xl">
+					{first_name_thai} {last_name_thai}
+				</h2>
 			</div>
 		</a>
 	);
